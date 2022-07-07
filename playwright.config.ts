bc42bba5072f4c26107bfb04393dc5786169e1b9
Gagan Suie <gagan.suie@codecrow.io>
2022-07-07 13:12:35 -0500

    Updated test directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 3000
-	}
+    testDir: 'tests',
+    webServer: {
+        command: 'npm run build && npm run preview',
+        port: 3000
+    }
 };
 
 export default config;
