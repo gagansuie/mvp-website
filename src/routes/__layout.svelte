267a161dd6b08f8d5ba4f0e2b831ef47b1deb492
aqeelchishti <git@github.com>
2022-08-08 00:51:51 +0500

    initial update 6

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import '../assets/app.css';
+	import '../assets/animate.css';
+	import '../assets/styles/tailwind-output.css';
+
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>MVP</title> />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css?family=Oxanium:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+		rel="stylesheet"
+	/>
+	<link
+		href="https://fonts.googleapis.com/css?family=RocknRoll+One:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+		rel="stylesheet"
+	/>
+	<link href="https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap" rel="stylesheet">
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/all.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/brands.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/duotone.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/fontawesome.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/light.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/regular.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/solid.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/codack04/github-cdn-service/font-awesome/6.1.0/css/thin.min.css"
+	/>
+</svelte:head>
+
+
+<main class="flex flex-col mx-auto w-full min-h-screen font-oxanium items-center bg-light-200">
+	<Header />
+	<slot />
+	<Footer />
+</main>
+
---
+++ src/routes/team.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import TeamMember from '$lib/components/TeamMember.svelte';
+</script>
+
+<section class="py-6 bg-light-200 text-main-200">
+	<div class="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
+		<h1 class="text-4xl font-extrabold leading-none text-center sm:text-4xl">
+			Active Team Members
+		</h1>
+		<svg
+			style="margin-top: 12px;"
+			class="text-center"
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			x="0px"
+			y="0px"
+			width="125px"
+			height="5px"
+			viewBox="0 0 124.985 3.411"
+			enable-background="new 0 0 124.985 3.411"
+			xml:space="preserve"
+		>
+			<polygon
+				fill="#ff8503"
+				points="0,0 124.985,0 124.985,1.121 96.484,1.121 86.944,3.411 38.67,3.411 29.162,1.121 0,1.121"
+			/>
+		</svg>
+		<p class="max-w-2xl text-center dark:dark:text-gray-400">
+			At a assumenda quas cum earum ut itaque commodi saepe rem aspernatur quam natus quis nihil
+			quod, hic explicabo doloribus magnam neque, exercitationem eius sunt!
+		</p>
+		<div class="flex flex-row flex-wrap-reverse justify-center">
+			<TeamMember
+				name="Leroy Jenkins"
+				avatarLink="https://source.unsplash.com/100x100/?portrait?0"
+				description="Nam commodo turpis id."
+				jobTitle="Visual Designer"
+				currentGame="Overwatch"
+			/>
+			<TeamMember
+				name="Leroy Jenkins"
+				avatarLink="https://source.unsplash.com/100x100/?portrait?0"
+				description="Nam commodo turpis id."
+				jobTitle="Visual Designer"
+				currentGame="Overwatch"
+			/>
+			<TeamMember
+				name="Leroy Jenkins"
+				avatarLink="https://source.unsplash.com/100x100/?portrait?0"
+				description="Nam commodo turpis id."
+				jobTitle="Visual Designer"
+				currentGame="Overwatch"
+			/>
+			<TeamMember
+				name="Leroy Jenkins"
+				avatarLink="https://source.unsplash.com/100x100/?portrait?0"
+				description="Nam commodo turpis id."
+				jobTitle="Visual Designer"
+				currentGame="Overwatch"
+			/>
+		</div>
+	</div>
+</section>
