205a155caf9f2b5b803e45b2342469574154dacd
Mandar Devarshi <mandardevarshi@gmail.com>
2022-11-09 02:36:56 +0530

    fix: old yarn manager migrated to latest and other fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-//import adapter from '@sveltejs/adapter-auto';
-
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,6 +11,7 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		
 	},
 	
 };
